5eff930fd4d74e766377fa2a8f58c0c2fe1a9e6e
Andrew <Caballiero777@gmail.com>
2016-06-12 14:02:08 +0300

    Final Version 1.0

--- Citizens/SimpleRegistry.cs
+++ Citizens/SimpleRegistry.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Linq;
     using Humanizer;
-    using System.Threading;
 
     public class SimpleRegistry : ICitizenRegistry
     {
@@ -11,33 +10,37 @@
 
         private int[] genderCounts = new int[2];
 
-        public ICitizen this[string id]
-        {
-            get
-            {
-                if(String.IsNullOrEmpty(id))
-                {
-                    throw new ArgumentNullException();
-                }
-                foreach(Citizen c in citizens)
-                    if(c!=null)
-                    {
-                        if(c.VatId == id)
-                        {
-                            return c;
-                        }
-                    }
-                return null;
-            }
-        }
-
         private DateTime? lastRegistrationDateTime;
 
         private int counterForAddNewCitizen = 0;
 
+        public ICitizen this[string id]
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(id))
+                {
+                    throw new ArgumentNullException();
+                }
+
+                foreach (Citizen c in this.citizens)
+                {
+                    if (c != null)
+                    {
+                        if (c.VatId == id)
+                        {
+                            return c;
+                        }
+                    }
+                }
+
+                return null;
+            }
+        }
+
         public void Register(ICitizen citizen)
         {
-            if (citizens.Contains(citizen, new Helpers.CitizenEqualsComparer()))
+            if (this.citizens.Contains(citizen, new Helpers.CitizenEqualsComparer()))
             {
                 throw new InvalidOperationException("You already add this citizen in registry.");
             }
@@ -53,61 +56,61 @@
                 newCitizen.VatId = citizen.VatId;
             }
 
-            for (int i = 0; i < citizens.Length; ++i)
+            for (int i = 0; i < this.citizens.Length; ++i)
             {
-                if(citizens[i] == null)
+                if (this.citizens[i] == null)
                 {
-                    citizens[i] = newCitizen;
-                    genderCounts[(int)newCitizen.Gender]++;
-                    lastRegistrationDateTime = SystemDateTime.Now();
+                    this.citizens[i] = newCitizen;
+                    this.genderCounts[(int)newCitizen.Gender]++;
+                    this.lastRegistrationDateTime = SystemDateTime.Now();
                     break;
                 }
             }
 
-            if(citizens[citizens.Length - 1] != null)
+            if (this.citizens[this.citizens.Length - 1] != null)
             {
-                DoubleCitizens();
+                this.DoubleCitizens();
             }
-        }
-
-        private void DoubleCitizens()
-        {
-            var newCitizens = new ICitizen[citizens.Length + 20];
-            for (int i = 0; i < citizens.Length; i++)
-            {
-                newCitizens[i] = citizens[i];
-            }
-
-            counterForAddNewCitizen += 20;
-        }
-
-        public string CreateId(ICitizen citizen)
-        {
-            var id = String.Empty;
-
-            id += new string(Convert.ToString((citizen.BirthDate - new DateTime(1899, 12, 31)).Days + 100000).ToArray(),1,5);
-
-            var count = citizens.Count(cit => cit?.Gender == citizen.Gender) + 1;
-
-            id += new String(Convert.ToString((count - 1 + (int)citizen.Gender)* 2 - (int)citizen.Gender + 10000).Reverse().Take(4).Reverse().ToArray());
-
-            id += Convert.ToString(((-Convert.ToInt32(id[0])
-                + Convert.ToInt32(id[1]) * 5
-                + Convert.ToInt32(id[2]) * 7
-                + Convert.ToInt32(id[3]) * 9
-                + Convert.ToInt32(id[4]) * 4
-                + Convert.ToInt32(id[5]) * 6
-                + Convert.ToInt32(id[6]) * 10
-                + Convert.ToInt32(id[7]) * 5
-                + Convert.ToInt32(id[8]) * 7)%11)%10);
-
-            return id;
         }
 
         public string Stats()
         {
-            var s = lastRegistrationDateTime.HasValue ? ". Last registration was " + lastRegistrationDateTime.Value.Humanize(dateToCompareAgainst:SystemDateTime.Now()) : "";
-            return $"{genderCounts[(int)Gender.Male]} m{(char)(97 + Convert.ToInt32(genderCounts[(int)Gender.Male] != 1) * 4)}n and {genderCounts[(int)Gender.Female]} wom{(char)(97 + Convert.ToInt32(genderCounts[(int)Gender.Female] != 1) * 4)}n" + s;
+            var s = this.lastRegistrationDateTime.HasValue ? ". Last registration was " + this.lastRegistrationDateTime.Value.Humanize(dateToCompareAgainst: SystemDateTime.Now()) : string.Empty;
+            return $"{this.genderCounts[(int)Gender.Male]} m{(char)(97 + (Convert.ToInt32(this.genderCounts[(int)Gender.Male] != 1) * 4))}n and {this.genderCounts[(int)Gender.Female]} wom{(char)(97 + (Convert.ToInt32(this.genderCounts[(int)Gender.Female] != 1) * 4))}n" + s;
+        }
+
+        private void DoubleCitizens()
+        {
+            var newCitizens = new ICitizen[this.citizens.Length + 20];
+            for (int i = 0; i < this.citizens.Length; i++)
+            {
+                newCitizens[i] = this.citizens[i];
+            }
+
+            this.counterForAddNewCitizen += 20;
+        }
+
+        private string CreateId(ICitizen citizen)
+        {
+            var id = string.Empty;
+
+            id += new string(Convert.ToString((citizen.BirthDate - new DateTime(1899, 12, 31)).Days + 100000).ToArray(), 1, 5);
+
+            var count = this.citizens.Count(cit => cit?.Gender == citizen.Gender) + 1;
+
+            id += new string(Convert.ToString(((count - 1 + (int)citizen.Gender) * 2) - (int)citizen.Gender + 10000).Reverse().Take(4).Reverse().ToArray());
+
+            id += Convert.ToString((-Convert.ToInt32(id[0])
+                + (Convert.ToInt32(id[1]) * 5)
+                + (Convert.ToInt32(id[2]) * 7)
+                + (Convert.ToInt32(id[3]) * 9)
+                + (Convert.ToInt32(id[4]) * 4)
+                + (Convert.ToInt32(id[5]) * 6)
+                + (Convert.ToInt32(id[6]) * 10)
+                + (Convert.ToInt32(id[7]) * 5)
+                + ((Convert.ToInt32(id[8]) * 7) % 11)) % 10);
+
+            return id;
         }
     }
 }
